b3d34f6d55cee37ebb5d7a9c59aefb0201fe2698
ckhsc17 <94750792+Bowenbowenbowenbowen@users.noreply.github.com>
2025-04-13 23:49:36 +0800

    test_flask

--- linebot-backend/test.py
+++ linebot-backend/test.py
@@ -1,20 +1,9 @@
-# flask test
 from flask import Flask, request, abort
-
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 
-from datetime import datetime, timedelta
-
 import os
-
-######################################################
 
 app = Flask(__name__)
 
@@ -22,76 +11,26 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
 
-# 監聽所有來自 /callback 的 Post Request
+# 監聽來自 /callback 的 POST 請求
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-    return 'OK'
+    return 'OK', 200
 
+# ✅ 改成 v1 SDK 的事件處理方式
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text in ["預約", "預約療程", "我要預約"]:
-        flex_message = FlexSendMessage(
-            alt_text="立即預約療程",
-            contents={
-                "type": "bubble",
-                "hero": {
-                    "type": "image",
-                    #"url": "https://your-clinic.com/logo.png",
-                    "size": "full",
-                    "aspectRatio": "20:13",
-                    "aspectMode": "cover"
-                },
-                "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                        {
-                            "type": "text",
-                            "text": "歡迎預約療程",
-                            "weight": "bold",
-                            "size": "xl"
-                        },
-                        {
-                            "type": "text",
-                            "text": "立即線上預約，專人為您服務",
-                            "size": "sm",
-                            "color": "#999999",
-                            "margin": "md"
-                        }
-                    ]
-                },
-                "footer": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                        {
-                            "type": "button",
-                            "style": "primary",
-                            "action": {
-                                "type": "uri",
-                                "label": "立即預約",
-                                #"uri": "https://your-nextjs-frontend.com/booking"
-                            }
-                        }
-                    ],
-                    "flex": 0
-                }
-            }
-        )
-        line_bot_api.reply_message(event.reply_token, flex_message)
-    else:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="您好，請輸入「預約」來開始預約療程 💆‍♀️")
-        )
+    # 回覆使用者剛傳來的文字（Echo Bot）
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
