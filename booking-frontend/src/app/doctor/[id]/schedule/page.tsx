44326f9a91c1500d6e9efa27c7a36d6fa6000e1d
ckhsc17 <94750792+Bowenbowenbowenbowen@users.noreply.github.com>
2025-06-04 22:05:44 +0800

    turn fetch url to production 3

--- booking-frontend/src/app/doctor/[id]/schedule/page.tsx
+++ booking-frontend/src/app/doctor/[id]/schedule/page.tsx
@@ -39,7 +39,7 @@ export default function SchedulePage() {
     }
 
     fetch(
-      `http://localhost:8000/api/doctors/availability?doctor_id=${doctorId}`
+      `https://booking-backend-prod-260019038661.asia-east1.run.app/api/doctors/availability?doctor_id=${doctorId}`
     )
       .then((res) => res.json())
       .then((data: { available_times: AvailableTime[] }) => {
