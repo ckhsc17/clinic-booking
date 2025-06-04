54621a18228da330d78021e88cc16c165e1f16b2
ckhsc17 <94750792+Bowenbowenbowenbowen@users.noreply.github.com>
2025-06-05 02:18:34 +0800

    https test

--- booking-frontend/src/app/doctor/[id]/schedule/page.tsx
+++ booking-frontend/src/app/doctor/[id]/schedule/page.tsx
@@ -41,7 +41,7 @@ export default function SchedulePage() {
     console.log("patientId:", patientId)
 
     fetch(
-      `https://booking-backend-prod-260019038661.asia-east1.run.app/api/doctors/availability/?doctor_id=${doctorId}&patient_id=${patientId}`
+      `https://booking-backend-prod-260019038661.asia-east1.run.app/api/doctors/availability?doctor_id=${doctorId}&patient_id=${patientId}`
     )
       .then((res) => res.json())
       .then((data: { available_times: AvailableTime[] }) => {
